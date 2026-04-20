a51ed590b3da5b52774725ff9bc67405361e01d5
bellaayang <jingjingyjh@126.com>
2026-04-20 09:20:05 +0800

    new: 46, 47

--- medium/47.permutations-ii.java
+++ medium/47.permutations-ii.java
@@ -1,11 +1,12 @@
 /*
- * @lc app=leetcode id=46 lang=java
+ * @lc app=leetcode id=47 lang=java
  *
- * [46] Permutations
+ * [47] Permutations II
  */
 
 // @lc code=start
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,13 +14,12 @@ class Solution {
     List<Integer> path = new LinkedList<>();
     List<List<Integer>> result = new LinkedList<>();
     boolean[] used;
-    
-    
-    public List<List<Integer>> permute(int[] nums) {
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
         used = new boolean[nums.length];
         backtracking(nums);
-        return result;
-         
+        return result;    
     }
 
     private void backtracking (int[] nums) {
@@ -27,18 +27,23 @@ class Solution {
             result.add(new LinkedList<>(path));
             return;
         }
+
         for (int i = 0; i < nums.length; i++) {
             if (used[i] == true) {
                 continue;
             }
+
+           if (i > 0 && nums[i] == nums[i - 1] && used[i - 1] == false) {
+                continue;
+            }
+
             path.add(nums[i]);
             used[i] = true;
             backtracking(nums);
-            used[i] = false;
-            path.removeLast();       
+            used[i] = false; 
+            path.removeLast();     
         }
     }
-    
 
     
 }
