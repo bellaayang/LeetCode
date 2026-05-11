4cca0dbc5b192460e394006e1137f40cd1aff88d
bellaayang <jingjingyjh@126.com>
2026-05-11 08:53:35 +0800

    2026.5.7 new: 42, 937

---
+++ hard/42.trapping-rain-water.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=42 lang=java
+ *
+ * [42] Trapping Rain Water
+ */
+
+// @lc code=start
+class Solution {
+    public int trap(int[] height) {
+        int leftIndex = 0;
+        int rightIndex = height.length - 1;
+
+        int leftMax = height[leftIndex];
+        int rightMax = height[rightIndex];
+
+        int result = 0;
+
+        while (leftIndex < rightIndex) {
+            if (leftMax < rightMax) {
+                result += Math.max(0, leftMax - height[leftIndex]);
+                leftIndex++;
+                leftMax = Math.max(leftMax, height[leftIndex]);
+            } else {
+                result += Math.max(0, rightMax - height[rightIndex]);
+                rightIndex--;
+                rightMax = Math.max(rightMax, height[rightIndex]);
+            }
+        }
+
+        return result;
+        
+    }
+}
+// @lc code=end
+
--- medium/937.reorder-data-in-log-files.java
+++ medium/937.reorder-data-in-log-files.java
@@ -5,8 +5,45 @@
  */
 
 // @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
+        List<String> letter = new ArrayList<>();
+        List<String> digit = new ArrayList<>();
+
+        for (String log: logs) {
+            int idx = log.indexOf(' ');
+            char x = log.charAt(idx + 1);
+            if (Character.isDigit(x)) {
+                digit.add(log);
+            } else {
+                letter.add(log);
+            }
+        }
+
+        letter.sort((a, b) -> {
+            int aIdx = a.indexOf(' ');
+            int bIdx = b.indexOf(' ');
+
+            String aFirst = a.substring(0, aIdx);
+            String aLast = a.substring(aIdx + 1);
+
+            String bFirst = b.substring(0, bIdx);
+            String bLast = b.substring(bIdx + 1);
+
+            int cmp = aLast.compareTo(bLast);
+            if (cmp != 0) {
+                return cmp;
+            }
+            return aFirst.compareTo(bFirst);
+
+        });
+
+       letter.addAll(digit);
+       return letter.toArray(new String[0]);
         
     }
 }
