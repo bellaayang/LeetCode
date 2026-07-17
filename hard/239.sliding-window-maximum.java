89a775c59248537f400ffa81430b4fa833320fdf
bellaayang <jingjingyjh@126.com>
2026-07-17 23:33:53 +0800

    2026.7.17

---
+++ hard/239.sliding-window-maximum.java
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=239 lang=java
+ *
+ * [239] Sliding Window Maximum
+ */
+
+// @lc code=start
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> dq = new ArrayDeque<>();
+        int[] result = new int[nums.length - k + 1];
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            while (!dq.isEmpty() && nums[dq.getLast()] < nums[right]) {
+                dq.removeLast();
+            }
+
+            dq.addLast(right);
+            if (left > dq.getFirst()) {
+                dq.removeFirst();
+            }
+
+            if (right + 1 >= k) {
+                result[left] = nums[dq.getFirst()];
+                left++;
+            }
+
+        }
+
+        return result;
+        
+    }
+}
+// @lc code=end
+
